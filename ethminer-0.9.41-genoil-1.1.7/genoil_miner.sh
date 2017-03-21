1c87b386c8085d37eb8dd807d5f2683509ff7175
stubbie6 <stubbie@gmail.com>
2017-03-21 19:19:08 +1100

    Add files via upload

---
+++ ethminer-0.9.41-genoil-1.1.7/genoil_miner.sh
@@ -0,0 +1,7 @@
+#Stratum mode using Genoils Miner
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+./ethminer.exe --farm-recheck 200 -G -S eu1.ethpool.org:3333 -FS us1.ethpool.org:3333 -O 0x660AD78dE51977CdC76b729Bf7EC15F99dD73066.Davidv --opencl-device 1
